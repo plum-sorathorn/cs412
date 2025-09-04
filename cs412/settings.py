f73fba8e648c85c85b6435458d606b3ad263f7aa
Sorathorn Thongpitukthavorn <plum@bu.edu>
2025-09-04 14:51:35 -0400

    3rd try for css file

--- cs412/settings.py
+++ cs412/settings.py
@@ -131,5 +131,5 @@ import socket
 CS_DEPLOYMENT_HOSTNAME = 'cs-webapps.bu.edu'
 
 if socket.gethostname() == CS_DEPLOYMENT_HOSTNAME:
-    STATIC_URL = '/plum/static/'
-    MEDIA_URL = '/plum/media/'
+    STATIC_URL = '/plum/django/static/'
+    MEDIA_URL = '/plum/django/media/'
