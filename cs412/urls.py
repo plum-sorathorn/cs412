005aa7e4f7d7048affa74eba814d3795b84f8adc
Sorathorn Thongpitukthavorn <plum@bu.edu>
2025-09-10 11:49:55 -0400

    changed up urls, and reference pointers so now nav bar works. Show all page also works.

--- cs412/urls.py
+++ cs412/urls.py
@@ -23,6 +23,4 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include("quotes.urls")),
-    path('quotes/', include("quotes.urls")),
-    path('show_all/', include("quotes.urls")),
 ]
--- quotes/urls.py
+++ quotes/urls.py
@@ -7,6 +7,7 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
-    path(r'', views.quotes, name="quotes")
-    path(r'', views.show_all, name="show_all")
+    path(r'', views.quotes, name="quotes"),
+    path(r'quotes/', views.quotes, name="quotes"),
+    path(r'show_all/', views.show_all, name="show_all"),
 ]
