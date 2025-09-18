c6384ba831d53bb0f6d8dd9fa8220e374ddb51b5
Sorathorn Thongpitukthavorn <plum@bu.edu>
2025-09-18 11:26:57 -0400

    updated views

--- restaurant/views.py
+++ restaurant/views.py
@@ -44,52 +44,6 @@ def order(request):
 
     return render(request, template_name, payload)
 
-# File: views.py
-# Author: Sorathorn Thongpitukthavorn (plum@bu.edu), 9/11/2025
-# Description:  logic of restaurant webapp. Includes HTTP POST
-#               methods and the whole menu. Will receive and process
-#               orders and return them as confirmation.
-
-from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse
-import random
-
-appetizers = ["Spring Rolls", "Honey Ribs", "Crying Wings (Spicy Wings)"]
-main_dishes = ["Crispy Tamarind Duck", "Thai Omelet with minced pork", "Seafood Stir-fried Curry"]
-desserts = ["Coconut Ice Cream", "Thai Pumpkin Custard", "Mango with Sticky Rice"]
-special_menus = [
-    "Crying Tiger (beef brisket)", "Crying Pork (grilled pork neck)", "Basil Fried Rice",
-]
-
-def main(request):
-    ''' Main webpage for the restaurant. Simply returns the page. '''
-
-    template_name = "main.html"
-
-    payload = {
-        "restaurant_pic" : "restaurant_pic.png",
-    }
-
-    return render(request, template_name, payload)
-
-def order(request):
-    ''' Order page. Will display the menu and allow the user to order.
-        Also has a daily special function which an item is chosen to be
-        displayed randomly. '''
-    
-    daily_special = random.choice(special_menus)
-
-    payload = {
-        'appetizers' : appetizers,
-        'mains' : main_dishes,
-        'desserts' : desserts,
-        'daily_special' : daily_special,
-    }
-    
-    template_name = "order.html"
-
-    return render(request, template_name, payload)
-
 def confirmation(request):
     ''' Confirmation page which will be opened after an order
         is submitted. Will process the submitted order and 
@@ -98,11 +52,6 @@ def confirmation(request):
     template_name = "confirmation.html"
 
     if request.POST:
-        # Get customer information from the form
-        customer_name = request.POST.get("name", "N/A")
-        customer_phone = request.POST.get("phone", "N/A")
-        customer_email = request.POST.get("email", "N/A")
-        special_instructions = request.POST.get("special_instructions", "N/A")
 
         # Get all selected items using getlist()
         selected_appetizers = request.POST.getlist("appetizer")
@@ -121,6 +70,14 @@ def confirmation(request):
         spice_level = None
         if "Crying Wings (Spicy Wings)" in ordered_items:
             spice_level = request.POST.get("spice", "Not specified")
+
+        # special instructions
+        special_instructions = request.POST.get("special_instructions", "N/A")
+
+        # Get customer information from the form
+        customer_name = request.POST.get("name", "N/A")
+        customer_phone = request.POST.get("phone", "N/A")
+        customer_email = request.POST.get("email", "N/A")
 
         payload = {
             'customer_name': customer_name,
