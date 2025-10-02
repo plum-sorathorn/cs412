2fad0e714fded5aabaf3b4e64e4024db8c76e06e
Sorathorn Thongpitukthavorn <plum@bu.edu>
2025-10-02 16:19:14 -0400

    add comment to forms.py

--- mini_insta/forms.py
+++ mini_insta/forms.py
@@ -1,3 +1,7 @@
+# File: forms.py
+# Author: Sorathorn Thongpitukthavorn (plum@bu.edu), 9/23/2025
+# Description: contains the class for the form to create posts
+
 from django import forms
 from .models import *
 
