ddc1ff0a8b4e2ade893297a9fb05efc9069902a1
Sorathorn Thongpitukthavorn <plum@bu.edu>
2025-10-29 14:48:55 -0400

    voter_analytics task 1 complete

---
+++ voter_analytics/urls.py
@@ -0,0 +1,7 @@
+# File: urls.py
+# Author: Sorathorn Thongpitukthavorn (plum@bu.edu), 9/23/2025
+# Description: url paths for voter_analytics webapp
+
+from django.urls import path   
+
+urlpatterns = []
