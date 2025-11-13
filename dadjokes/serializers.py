f3d5cfd79d2f3c9bd1d02b12a4505e0364b5b3bb
Sorathorn Thongpitukthavorn <plum@bu.edu>
2025-11-13 10:38:01 -0500

    backend api done.

--- dadjokes/serializers.py
+++ dadjokes/serializers.py
@@ -17,6 +17,11 @@ class JokeSerializer(serializers.ModelSerializer):
         model = Joke 
         fields = ['text','name','timestamp']
 
+    def create(self, validated_data):
+        joke = Joke.object.create(**validated_data)
+        joke.save()
+        return joke 
+
 class PictureSerializer(serializers.ModelSerializer):
     ''' 
     A serializer class for the Picture model
@@ -27,16 +32,15 @@ class PictureSerializer(serializers.ModelSerializer):
         model = Picture
         fields = ['image_url','name','timestamp']
 
-def create(self, validated_data):
-    ''' handle object creation '''
-
-    # create a Joke object
-    if validated_data["text"]:
-        joke = Joke.object.create(**validated_data)
-        joke.save()
-        return joke 
-    else:
+    def create(self, validated_data):
+        ''' handle object creation '''
         picture = Picture.object.create(**validated_data)
         picture.save()
         return picture
-        
+
+class BothSerializer(serializers.Serializer):
+    """
+    A serializer to combine the random Joke and random Picture data.
+    """
+    joke = JokeSerializer()
+    picture = PictureSerializer()
--- dadjokes/urls.py
+++ dadjokes/urls.py
@@ -3,9 +3,13 @@
 # Description: url paths for dadjokes api
 
 from django.urls import path   
-from .views import ListCreateJokeAPIView, ListCreatePictureAPIView
+from .views import *
 
 urlpatterns = [
+    path('api/', OneJokePictureAPIView.as_view(), name="joke_picture_api"),
+    path('api/random', OneJokePictureAPIView.as_view(), name="random_joke_picture_api"),
     path('api/jokes', ListCreateJokeAPIView.as_view(), name="create_list_joke_api"),
+    path('api/joke/<pk>', PrimaryJokeAPIView.as_view(), name="primary_joke_api"),
     path('api/pictures', ListCreatePictureAPIView.as_view(), name="create_list_picture_api"),
+    path('api/picture/<pk>', PrimaryPictureAPIView.as_view(), name="primary_picture_api"),
 ]
